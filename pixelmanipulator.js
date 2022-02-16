29d407347e621cd54cd5495d5d5f8cbd45c64b9f
Nathan Fritzler <nfblaster@live.com>
2022-02-15 21:23:51 -0700

    Use request animation frame
    Closes #19

--- pixelmanipulator.js
+++ pixelmanipulator.js
@@ -358,7 +358,7 @@
 				//console.log("play");
 				if (innerP.mode==="playing") innerP.reset(canvasSizes);
 				innerP.mode="playing";
-				innerP.loopint=setInterval(innerP.iterate,1);
+				innerP.loopint=g.requestAnimationFrame(innerP.iterate);
 			},
 			reset:function(canvasSizes) {//reset (and resize) the canvas(es)
 				//        ({}?        )
@@ -386,7 +386,7 @@
 			},
 			pause:function() {//pause canvas iterations
 				innerP.mode="paused";
-				clearInterval(innerP.loopint);
+				g.cancelAnimationFrame(innerP.loopint);
 			},
 			zoom:function(e  ) {//This tells pixelmanipulator where to focus the center of the zoomElm
 				//       ({}?)
@@ -611,6 +611,8 @@
 				}
 				innerP.update();
 				innerP.onAfterIterate();
+				if (innerP.mode==="playing")
+					innerP.loopint=g.requestAnimationFrame(innerP.iterate);
 			},
 			updateData:function() {//defines the starting values of the library and is run on `p.reset();`
 				//console.log("updateData");
