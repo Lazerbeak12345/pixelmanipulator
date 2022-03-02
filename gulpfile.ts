d26801eb96fb99c68facfda8485f6e3c603a44f8
Nathan Fritzler <nfblaster@live.com>
2022-03-01 20:38:04 -0700

    Initial attempt at using babel's builtin typescript
    Probabbly not the best way to go about this.
    
    Related to #53

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,17 +1,27 @@
-import { watch, src, dest, series } from 'gulp'
+import { watch, src, dest, series, parallel } from 'gulp'
 import { createProject } from 'gulp-typescript'
+import babel from 'gulp-babel'
 import { rollup } from 'rollup'
 type Stream=NodeJS.ReadWriteStream
 const tsProject = createProject('tsconfig.json')
 const sourceGlob = 'src/lib/*'
-export function buildEs (): Stream {
+export function buildTypes (): Stream {
   return src(sourceGlob)
     .pipe(tsProject())
     .pipe(dest('dist/lib'))
 }
+export function buildEsOnly (): Stream {
+  return src(sourceGlob)
+    .pipe(babel())
+    .pipe(dest('dist/lib'))
+}
+export const buildEs = parallel(buildTypes, buildEsOnly)
 async function rollupToUmd (): Promise<void> {
   const bundle = await rollup({
     input: 'dist/lib/pixelmanipulator.js'
+    // For now, rollup won't try to resolve non-local imports anyway, so
+    // it will just give an error message.
+    // external: [/node_modules/]
   })
   await bundle.write({
     file: 'dist/umd/pixelmanipulator.js',
