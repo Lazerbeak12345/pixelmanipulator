2611388969aee5660825ff604ee24e1a19718df7
Nathan Fritzler <nfblaster@live.com>
2022-03-02 22:28:28 -0700

    Move all element grabs to top of file

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -3,7 +3,57 @@ import { PixelManipulator, Color, version } from '../lib/pixelmanipulator.js'
 const p: PixelManipulator = new PixelManipulator()
 const timedebug = true
 let framecount = 0
-const selectorboxSty = document.getElementById('selectorBox')?.style
+
+const altFill = document.getElementById('altFill')
+const altFillP = document.getElementById('altFillP')
+const altSelect = document.getElementById('altSelect')
+const backendversion = document.getElementById('backendversion')
+const canvas = document.getElementById('canvas')
+const ctrlFill = document.getElementById('ctrlFill')
+const ctrlFillP = document.getElementById('ctrlFillP')
+const ctrlSelect = document.getElementById('ctrlSelect')
+const customizeT = document.getElementById('customizeT')
+const customizeColor = document.getElementById('customizeColor')
+const customizeColorAlpha = document.getElementById('customizeColorAlpha')
+const customizeColorAlphaText = document.getElementById('customizeColorAlphaText')
+const customizeLoop = document.getElementById('customizeLoop')
+const customizeName = document.getElementById('customizeName')
+const customizeSection = document.getElementById('customizePatternSection')
+const customizer = document.getElementById('customizer')
+const customSelect = document.getElementById('customSelect')
+const heightE = document.getElementById('height')
+const largexline1Elm = document.getElementById('largexline1')
+const largexlineElm = document.getElementById('largexline')
+const largeyline1Elm = document.getElementById('largeyline1')
+const largeylineElm = document.getElementById('largeyline')
+const normalFill = document.getElementById('normalFill')
+const normalFillP = document.getElementById('normalFillP')
+const normalSelect = document.getElementById('normalSelect')
+const oneFrameAtATime = document.getElementById('oneFrameAtATime')
+const pauseBtn = document.getElementById('pause')
+const pixelCounter = document.getElementById('pixelCounter')
+const pixelCounterT = document.getElementById('pixelCounterT')
+const playBtn = document.getElementById('play')
+const resetBtn = document.getElementById('reset')
+const selectorBoxElm = document.getElementById('selectorBox')
+const shfocusboxElm = document.getElementById('shfocusbox')
+const shtargeterElm = document.getElementById('shtargeter')
+const smallxline = document.getElementById('smallxline')
+const smallyline = document.getElementById('smallyline')
+const widthE = document.getElementById('width')
+const zoom = document.getElementById('zoom')
+const zhE = document.getElementById('zoomHeightElm')
+const zwE = document.getElementById('zoomWidthElm')
+
+const selectorboxSty = selectorBoxElm?.style
+const largexlinesty = largexlineElm?.style
+const largexline1sty = largexline1Elm?.style
+const largeyline1sty = largeyline1Elm?.style
+const largeylinesty = largeylineElm?.style
+
+const elmdrops = document.getElementsByClassName('elmDrop')
+
+
 function updateBox (): void {
   if (selectorboxSty != null && p.zoomelm != null) {
     selectorboxSty.width = `${p.zoomelm.width / p.zoomScaleFactor}px`
@@ -16,8 +66,6 @@ interface PageLoc{
   pageX: number
   pageY: number
 }
-const smallxline = document.getElementById('smallxline')
-const smallyline = document.getElementById('smallyline')
 function updateSmallLines (e: MouseEvent|PageLoc): void {
   if (smallxline != null && smallyline != null) {
     smallxline.style.left = `${e.pageX}px`
@@ -40,8 +88,6 @@ function updateSmallLines (e: MouseEvent|PageLoc): void {
     updateLargeLinesY(x, y)
   } else updateLargeLinesY(x, -1)
 }
-const largexlinesty = document.getElementById('largexline')?.style
-const largexline1sty = document.getElementById('largexline1')?.style
 function updateLargeLinesX (x: number, y: number): void {
   if (p.zoomelm == null) return
   const zh = p.zoomelm.height
@@ -84,7 +130,6 @@ function updateLargeLinesY (x: number, y: number): void {
     w2 = 0
   }
 
-  const largeylinesty = document.getElementById('largeyline')?.style
   if (largeylinesty != null) {
     largeylinesty.height = `${p.zoomScaleFactor}px`
     largeylinesty.width = `${w}px`
@@ -92,7 +137,6 @@ function updateLargeLinesY (x: number, y: number): void {
     largeylinesty.right = '0'
   }
 
-  const largeyline1sty = document.getElementById('largeyline1')?.style
   if (largeyline1sty != null) {
     largeyline1sty.height = `${p.zoomScaleFactor}px`
     largeyline1sty.width = `${w2}px`
@@ -145,15 +189,9 @@ function hex2rgba (hex: string, alpha: number): Color|null {
   return output
 }
 function updateCustomizer (): void {
-  const customSelect = document.getElementById('customSelect')
   if (customSelect == null) return
   const elm = p.elementTypeMap.get((customSelect as HTMLSelectElement).value)
   if (elm == null) return
-  const customizeSection = document.getElementById('customizePatternSection')
-  const customizeColor = document.getElementById('customizeColor')
-  const customizeColorAlpha = document.getElementById('customizeColorAlpha')
-  const customizeColorAlphaText = document.getElementById('customizeColorAlphaText')
-  const customizeName = document.getElementById('customizeName')
   if (customizeColor != null) (customizeColor as HTMLInputElement).value = rgb2hex(elm.color)
   const alphaVal = (elm.color[3] ?? 255).toString()
   if (customizeColorAlpha != null) (customizeColorAlpha as HTMLInputElement).value = alphaVal// Raw alpha value
@@ -162,16 +200,12 @@ function updateCustomizer (): void {
   if (customizeSection != null) {
     if (typeof elm.pattern === 'string') {
       customizeSection.classList.remove('hidden')
-      const customizeLoop = document.getElementById('customizeLoop')
       if (customizeLoop != null) (customizeLoop as HTMLInputElement).checked = elm.loop ?? false
     } else customizeSection.classList.add('hidden')
   }
 }
-const backendversion = document.getElementById('backendversion')
 if (backendversion != null) backendversion.innerText = version
-const canvas = document.getElementById('canvas')
 if (canvas == null) throw new Error('canvas could not be found!')
-const zoom = document.getElementById('zoom')
 p.canvasPrep(zoom == null
   ? { canvas: canvas as HTMLCanvasElement }
   : {
@@ -182,20 +216,6 @@ if (p.zoomelm != null) {
   p.zoomelm.addEventListener('click', zoomClick)
   p.zoomelm.addEventListener('drag', zoomClick)
 }
-const selectorBoxElm = document.getElementById('selectorBox')
-const smallxlineElm = document.getElementById('smallxline')
-const smallylineElm = document.getElementById('smallyline')
-const largexlineElm = document.getElementById('largexline')
-const largexline1Elm = document.getElementById('largexline1')
-const largeylineElm = document.getElementById('largeyline')
-const largeyline1Elm = document.getElementById('largeyline1')
-const shtargeterElm = document.getElementById('shtargeter')
-const shfocusboxElm = document.getElementById('shfocusbox')
-const customizer = document.getElementById('customizer')
-const customizerS = document.getElementById('customSelect')
-const customizeLoop = document.getElementById('customizeLoop')
-const customizeColor = document.getElementById('customizeColor')
-const customizeColorAlpha = document.getElementById('customColorAlpha')
 canvas.addEventListener('click', updateBox)
 canvas.addEventListener('click', (event) =>
   p.zoom({
@@ -208,24 +228,23 @@ if (selectorBoxElm != null) {
   selectorBoxElm.addEventListener('mousemove', boxHoverOrClick)
   selectorBoxElm.addEventListener('click', boxHoverOrClick)
 }
-if (smallxlineElm != null) {
-  smallxlineElm.addEventListener('mousemove', updateSmallLines)
-  smallxlineElm.addEventListener('click', selectorClicked)
+if (smallxline != null) {
+  smallxline.addEventListener('mousemove', updateSmallLines)
+  smallxline.addEventListener('click', selectorClicked)
 }
-if (smallylineElm != null) {
-  smallylineElm.addEventListener('mousemove', updateSmallLines)
-  smallylineElm.addEventListener('click', selectorClicked)
+if (smallyline != null) {
+  smallyline.addEventListener('mousemove', updateSmallLines)
+  smallyline.addEventListener('click', selectorClicked)
 }
 if (p.zoomelm != null) p.zoomelm.addEventListener('mousemove', updateBothLargeLines)
 if (largexlineElm != null) largexlineElm.addEventListener('mousemove', bigLineGotHovered)
 if (largexline1Elm != null) largexline1Elm.addEventListener('mousemove', bigLineGotHovered)
 if (largeylineElm != null)largeylineElm.addEventListener('mousemove', bigLineGotHovered)
 if (largeyline1Elm != null)largeyline1Elm.addEventListener('mousemove', bigLineGotHovered)
-const customizeName = document.getElementById('customizeName')
 if (customizeName != null) {
   customizeName.addEventListener('change', function (this: HTMLInputElement) {
     console.log('change name', this.value)
-    const num = p.elementTypeMap.get((customizerS as HTMLSelectElement).value)?.number
+    const num = p.elementTypeMap.get((customSelect as HTMLSelectElement).value)?.number
     if (num != null) {
       p.modifyElement(num, {
         name: this.value
@@ -236,7 +255,7 @@ if (customizeName != null) {
 }
 function changeColor (): void {
   console.log('change color')
-  const num = p.elementTypeMap.get((customizerS as HTMLSelectElement).value)?.number
+  const num = p.elementTypeMap.get((customSelect as HTMLSelectElement).value)?.number
   const color = hex2rgba((customizeColor as HTMLInputElement).value, parseInt((customizeColorAlpha as HTMLInputElement).value))
   if (num != null && color != null) {
     p.modifyElement(num, {
@@ -249,7 +268,7 @@ if (customizeColorAlpha != null) customizeColorAlpha.addEventListener('change', 
 if (customizeLoop != null) {
   customizeLoop.addEventListener('change', function (this: HTMLInputElement) {
     console.group('change loop', this.checked)
-    const num = p.elementTypeMap.get((customizerS as HTMLSelectElement).value)?.number
+    const num = p.elementTypeMap.get((customSelect as HTMLSelectElement).value)?.number
     if (num != null) {
       p.modifyElement(num, {
         loop: this.checked
@@ -261,8 +280,8 @@ if (customizeLoop != null) {
 if (shtargeterElm != null) {
   shtargeterElm.addEventListener('click', function (this: HTMLInputElement) {
     const state = this.checked ? 'hidden' : 'visible'
-    if (smallxlineElm != null) smallxlineElm.style.visibility = state
-    if (smallylineElm != null) smallylineElm.style.visibility = state
+    if (smallxline != null) smallxline.style.visibility = state
+    if (smallyline != null) smallyline.style.visibility = state
     if (largexlineElm != null) largexlineElm.style.visibility = state
     if (largexline1Elm != null) largexline1Elm.style.visibility = state
     if (largeylineElm != null) largeylineElm.style.visibility = state
@@ -275,8 +294,7 @@ if (shfocusboxElm != null) {
     if (selectorBoxElm != null) selectorBoxElm.style.visibility = state
   })
 }
-if (customizerS != null) customizerS.addEventListener('change', () => updateCustomizer())
-const customizeT = document.getElementById('customizeT')
+if (customSelect != null) customSelect.addEventListener('change', () => updateCustomizer())
 if (customizeT != null) {
   customizeT.addEventListener('click', function (this: HTMLInputElement) {
     if (customizer == null) return
@@ -388,9 +406,6 @@ p.addMultipleElements({
     }
   }
 })
-const ctrlSelect = document.getElementById('ctrlSelect')
-const altSelect = document.getElementById('altSelect')
-const normalSelect = document.getElementById('normalSelect')
 function onZoomClick (e: MouseEvent, rel: {x: number, y: number}): string { // an event-like function that returns what should be set where the zoom ctx was clicked
   let active = ''
   if (e.ctrlKey && ctrlSelect != null) active = (ctrlSelect as HTMLSelectElement).value
@@ -415,8 +430,6 @@ p.onIterate = () => {
   if (timedebug) lasttime = performance.now()
   framecount++
 }
-const pixelCounterT = document.getElementById('pixelCounterT')
-const pixelCounter = document.getElementById('pixelCounter')
 if (pixelCounterT != null && pixelCounter != null) {
   p.onAfterIterate = () => {
     if (!(pixelCounterT as HTMLInputElement).checked) {
@@ -432,8 +445,6 @@ if (pixelCounterT != null && pixelCounter != null) {
   }
   pixelCounterT.addEventListener('click', p.onAfterIterate)
 }
-const playBtn = document.getElementById('play')
-const pauseBtn = document.getElementById('pause')
 if (playBtn != null) {
   playBtn.addEventListener('click', function (this: HTMLButtonElement) {
     p.play()
@@ -442,15 +453,9 @@ if (playBtn != null) {
   });
   (playBtn as HTMLButtonElement).disabled = false
 }
-const resetBtn = document.getElementById('reset')
-const oneFrameAtATime = document.getElementById('oneFrameAtATime')
 if (oneFrameAtATime != null) {
   oneFrameAtATime.addEventListener('click', () => p.iterate())
 }
-const widthE = document.getElementById('width')
-const heightE = document.getElementById('height')
-const zwE = document.getElementById('zoomWidthElm')
-const zhE = document.getElementById('zoomHeightElm')
 if (resetBtn != null) {
   resetBtn.addEventListener('click', () => {
     if (widthE != null && heightE != null) {
@@ -485,8 +490,6 @@ if (pauseBtn != null) {
     p.pause()
   })
 }
-const normalFill = document.getElementById('normalFill')
-const normalFillP = document.getElementById('normalFillP')
 if (normalFill != null && normalSelect != null && normalFillP != null) {
   normalFill.addEventListener('click', () => {
     p.randomlyFill(
@@ -496,8 +499,6 @@ if (normalFill != null && normalSelect != null && normalFillP != null) {
     p.update() // needed after any changes are made
   })
 }
-const ctrlFill = document.getElementById('ctrlFill')
-const ctrlFillP = document.getElementById('ctrlFillP')
 if (ctrlFill != null && ctrlSelect != null && ctrlFillP != null) {
   ctrlFill.addEventListener('click', () => {
     p.randomlyFill(
@@ -507,8 +508,6 @@ if (ctrlFill != null && ctrlSelect != null && ctrlFillP != null) {
     p.update()
   })
 }
-const altFill = document.getElementById('altFill')
-const altFillP = document.getElementById('altFillP')
 if (altFill != null && altSelect != null && altFillP != null) {
   altFill.addEventListener('click', () => {
     p.randomlyFill(
@@ -518,30 +517,25 @@ if (altFill != null && altSelect != null && altFillP != null) {
     p.update()
   })
 }
-const elmdrops = document.getElementsByClassName('elmDrop')
-const nsve = document.getElementById('normalSelect')
-const csve = document.getElementById('ctrlSelect')
-const asve = document.getElementById('altSelect')
-const cusve = document.getElementById('customSelect')
 p.onElementModified = () => {
   let nsv
   let csv
   let asv
   let cusv
-  if (nsve != null) {
-    nsv = (nsve as HTMLSelectElement).value
+  if (normalSelect != null) {
+    nsv = (normalSelect as HTMLSelectElement).value
     if (nsv.length === 0) nsv = "Conway's Game Of Life"
   }
-  if (csve != null) {
-    csv = (csve as HTMLSelectElement).value
+  if (ctrlSelect != null) {
+    csv = (ctrlSelect as HTMLSelectElement).value
     if (csv.length === 0) csv = 'Blocks'
   }
-  if (asve != null) {
-    asv = (asve as HTMLSelectElement).value
+  if (altSelect != null) {
+    asv = (altSelect as HTMLSelectElement).value
     if (asv.length === 0) asv = 'Water'
   }
-  if (cusve != null) {
-    cusv = (cusve as HTMLSelectElement).value
+  if (customSelect != null) {
+    cusv = (customSelect as HTMLSelectElement).value
     if (cusv.length === 0) cusv = "Conway's Game Of Life"
   }
   for (let i = 0; i < elmdrops.length; i++) {
@@ -554,13 +548,13 @@ p.onElementModified = () => {
     }
   }
   // Restore that selection, accounting for aliases
-  if (nsv != null) (nsve as HTMLSelectElement).value = p.getElementByName(nsv)?.name ?? ''
-  if (csv != null) (csve as HTMLSelectElement).value = p.getElementByName(csv)?.name ?? ''
-  if (asv != null) (asve as HTMLSelectElement).value = p.getElementByName(asv)?.name ?? ''
+  if (nsv != null) (normalSelect as HTMLSelectElement).value = p.getElementByName(nsv)?.name ?? ''
+  if (csv != null) (ctrlSelect as HTMLSelectElement).value = p.getElementByName(csv)?.name ?? ''
+  if (asv != null) (altSelect as HTMLSelectElement).value = p.getElementByName(asv)?.name ?? ''
   if (cusv != null) {
-    (cusve as HTMLSelectElement).value = p.getElementByName(cusv)?.name ?? ''
-    if ((cusve as HTMLSelectElement).value != null)updateCustomizer()
+    (customSelect as HTMLSelectElement).value = p.getElementByName(cusv)?.name ?? ''
+    if ((customSelect as HTMLSelectElement).value != null)updateCustomizer()
   }
 }
-if (cusve != null) p.onElementModified(parseInt((cusve as HTMLSelectElement).value)) // Call it once to fill in dropdowns
+if (customSelect != null) p.onElementModified(parseInt((customSelect as HTMLSelectElement).value)) // Call it once to fill in dropdowns
 // vim: tabstop=2 shiftwidth=2 expandtab
