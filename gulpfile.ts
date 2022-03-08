b4b4e2b9e84ad5e76ccfefb8ecf52fa553c1951f
Nathan Fritzler <nfblaster@live.com>
2022-03-07 21:50:37 -0700

    Start using babel
    Having serious thoughts about making a build folder...

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,7 +1,13 @@
 import { watch, src, dest, series, parallel } from 'gulp'
 import { createProject } from 'gulp-typescript'
 import * as typedoc from 'gulp-typedoc'
+import * as babel from 'gulp-babel'
+import terser from 'gulp-terser'
 import { rollup } from 'rollup'
+import resolve from '@rollup/plugin-node-resolve'
+// import commonjs from '@rollup/plugin-commonjs'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const commonjs = require('@rollup/plugin-commonjs')
 type Stream=NodeJS.ReadWriteStream
 const tsProject = createProject('tsconfig.json')
 const sourceGlob = 'src/lib/*'
@@ -20,22 +26,61 @@ export function buildEs (): Stream {
     .pipe(dest('dist/lib'))
 }
 export const buildEsAndDocs = parallel(buildEs, buildDocs)
+export function buildBabel (): Stream {
+  return src('dist/lib/*.js')
+    .pipe(babel())
+    .pipe(src('dist/lib/*.d.ts'))
+    .pipe(dest('dist/babel/'))
+}
+const umdOutput = 'dist/umd/pixelmanipulator.js'
 async function rollupToUmd (): Promise<void> {
   const bundle = await rollup({
-    input: 'dist/lib/pixelmanipulator.js'
+    input: 'dist/babel/pixelmanipulator.js',
+    external: /core-js/
   })
   await bundle.write({
-    file: 'dist/umd/pixelmanipulator.js',
+    file: umdOutput,
     format: 'umd',
     name: 'pixelmanipulator'
   })
   return await bundle.close()
 }
-function postRollup (): Stream {
-  return src('dist/lib/*.d.ts')
-    .pipe(dest('dist/umd'))
+const bundleOutput = 'dist/bundle/pixelmanipulator.js'
+async function rollupToBundle (): Promise<void> {
+  const bundle = await rollup({
+    input: 'dist/babel/pixelmanipulator.js',
+    plugins: [
+      resolve(),
+      commonjs()
+    ]
+  })
+  await bundle.write({
+    file: bundleOutput,
+    format: 'umd',
+    name: 'pixelmanipulator'
+  })
+  return await bundle.close()
 }
-export const buildUmd = series(buildEsAndDocs, rollupToUmd, postRollup)
+const doRollup = parallel(rollupToUmd, rollupToBundle)
+function minifyUmd (): Stream {
+  return src(umdOutput)
+    .pipe(terser())
+    .pipe(dest('dist/umd/min'))
+}
+function minifyBundle (): Stream {
+  return src(bundleOutput)
+    .pipe(terser())
+    .pipe(dest('dist/bundle/min'))
+}
+const minify = parallel(minifyUmd, minifyBundle)
+function moveRollupTypes (): Stream {
+  return src('dist/babel/*.d.ts')
+    .pipe(dest('dist/umd'))
+    .pipe(dest('dist/bundle'))
+    .pipe(dest('dist/umd/min'))
+    .pipe(dest('dist/bundle/min'))
+}
+export const buildUmd = series(buildEsAndDocs, buildBabel, doRollup, minify, moveRollupTypes)
 const demoSrcGlob = 'src/demo/*.ts'
 export function demoTsc (): Stream {
   return src(demoSrcGlob)
