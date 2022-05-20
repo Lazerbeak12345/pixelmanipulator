3d5ae6f407213b8dd0cb4de152973b3157531bce
Nathan Fritzler <nfblaster@live.com>
2022-05-20 17:01:49 -0600

    Simplify rect tests

--- tests/lib/neighborhoods/test-rect.ts
+++ tests/lib/neighborhoods/test-rect.ts
@@ -2,61 +2,33 @@ import { testProp, fc } from 'ava-fast-check'
 import { neighborhoods } from '../../../src/lib/pixelmanipulator'
 const { rect } = neighborhoods
 
-const wh = fc.nat({ max: 250 })
+// Maximums provided to ensure reasonable time and memory is spent
+const tlBr = fc.tuple(
+  fc.integer(),
+  fc.integer(),
+  fc.nat({ max: 250 }),
+  fc.nat({ max: 250 })
+).map(([x, y, w, h]) => [
+  { x, y },
+  {
+    x: x + w,
+    y: y + h
+  }
+])
 
-testProp(
-  'rect\'s size output is the area',
-  // Maximums provided to ensure reasonable time and memory is spent
-  [fc.integer(), fc.integer(), wh, wh],
-  (t, x, y, w, h) => {
-    const topLeft = {
-      x,
-      y
-    }
-    const bottomRight = {
-      x: x + w,
-      y: y + h
-    }
-    const expectedArea = (w + 1) * (h + 1) // (0,0) to (1,1) is area of 4
-    const list = rect(topLeft, bottomRight)
-    t.is(list.length, expectedArea)
-  }
-)
-testProp(
-  'return from rect has no duplicates',
-  [fc.integer(), fc.integer(), wh, wh],
-  (t, x, y, w, h) => {
-    const topLeft = {
-      x,
-      y
-    }
-    const bottomRight = {
-      x: x + w,
-      y: y + h
-    }
-    const list = rect(topLeft, bottomRight).map(pos => JSON.stringify(pos))
-    const set = new Set(list)
-    t.is(list.length, set.size)
-  }
-)
-testProp(
-  'return from rect keeps all values within bounds',
-  [fc.integer(), fc.integer(), wh, wh],
-  (t, x, y, w, h) => {
-    const topLeft = {
-      x,
-      y
-    }
-    const bottomRight = {
-      x: x + w,
-      y: y + h
-    }
-    const list = rect(topLeft, bottomRight)
-    t.plan(list.length * 2)
-    list.forEach(pos => {
-      t.true(topLeft.x <= pos.x && pos.x <= bottomRight.x)
-      t.true(topLeft.y <= pos.y && pos.y <= bottomRight.y)
-    })
-  }
-)
+testProp('rect\'s size output is the area', [tlBr], (t, [topLeft, bottomRight]) => {
+  // (0,0) to (1,1) is area of 4
+  const w = bottomRight.x - topLeft.x + 1
+  const h = bottomRight.y - topLeft.y + 1
+  t.is(rect(topLeft, bottomRight).length, w * h)
+})
+testProp('return from rect has no duplicates', [tlBr], (t, [topLeft, bottomRight]) => {
+  const list = rect(topLeft, bottomRight).map(pos => JSON.stringify(pos))
+  t.is(list.length, new Set(list).size)
+})
+testProp('return from rect keeps all values within bounds', [tlBr], (t, [topLeft, bottomRight]) =>
+  rect(topLeft, bottomRight).forEach(pos => {
+    t.true(topLeft.x <= pos.x && pos.x <= bottomRight.x)
+    t.true(topLeft.y <= pos.y && pos.y <= bottomRight.y)
+  }))
 // vi: tabstop=2 shiftwidth=2 expandtab
