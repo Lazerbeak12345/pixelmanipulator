0b9d11c81ee043fe26fde44eec637663be556708
Lazerbeak12345 <Lazerbeak12345@users.noreply.github.com>
2025-01-01 16:52:18 -0700

    feat: get proof-of-concept working for shared state

---
+++ packages/pixelmanipulator-web-demo/src/components/App.vue
@@ -0,0 +1,6 @@
+<script lang="ts" setup>
+import * as package_json from '../../package.json'
+</script>
+<template>
+	{{package_json.version}}
+</template>
---
+++ packages/pixelmanipulator-web-demo/src/components/GitHubBtn.vue
@@ -0,0 +1,7 @@
+<template>
+	<div>
+		<a target="_blank" href="https://github.com/Lazerbeak12345/pixelmanipulator">
+			<img alt="GitHub Repo" src="https://img.shields.io/github/stars/lazerbeak12345/pixelmanipulator?logo=github">
+		</a>
+	</div>
+</template>
