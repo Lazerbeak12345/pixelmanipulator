7f7a3aa1709bab1ba3bd5f4abeaa93b6e93b53dd
Lazerbeak12345 <Lazerbeak12345@users.noreply.github.com>
2025-01-01 23:08:36 -0700

    feat(web-demo): ShTargeter

---
+++ packages/pixelmanipulator-web-demo/src/components/ShTargeter.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { storeToRefs } from 'pinia'
+const props = defineProps([
+	// TODO: move state into better place
+	'useShTargeterStore',
+	// TODO: convert to emit
+	'change'
+]);
+const shTargeterStore = props.useShTargeterStore()
+const { checked } = storeToRefs(shTargeterStore)
+</script>
+<template>
+	<div class="form-check">
+		<input class="form-check-input" id="shtargeter" v-model="checked" type="checkbox">
+		<label class="form-check-label" for="shtargeter" title="Show targeter lines">
+			<i class="fa-solid fa-crosshairs" aria-hidden="true"></i>
+			Targeter Lines
+		</label>
+	</div>
+</template>
