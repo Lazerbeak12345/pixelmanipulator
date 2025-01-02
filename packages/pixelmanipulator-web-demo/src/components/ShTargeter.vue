a3e4997056f8687cb1c212a168634919d111eb06
Lazerbeak12345 <Lazerbeak12345@users.noreply.github.com>
2025-01-01 23:41:33 -0700

    fix(web-demo): ShTargeter

--- packages/pixelmanipulator-web-demo/src/components/ShTargeter.vue
+++ packages/pixelmanipulator-web-demo/src/components/ShTargeter.vue
@@ -12,7 +12,7 @@ const { checked } = storeToRefs(shTargeterStore)
 </script>
 <template>
 	<div class="form-check">
-		<input class="form-check-input" id="shtargeter" v-model="checked" type="checkbox">
+		<input class="form-check-input" id="shtargeter" v-model="checked" type="checkbox" @change="props.change(checked)">
 		<label class="form-check-label" for="shtargeter" title="Show targeter lines">
 			<i class="fa-solid fa-crosshairs" aria-hidden="true"></i>
 			Targeter Lines
