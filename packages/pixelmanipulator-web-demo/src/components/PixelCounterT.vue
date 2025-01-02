a2d694794b162922f4b47f64c615138c36dd8e14
Lazerbeak12345 <Lazerbeak12345@users.noreply.github.com>
2025-01-01 22:19:44 -0700

    feat(web-demo): ShFocusBox

--- packages/pixelmanipulator-web-demo/src/components/PixelCounterT.vue
+++ packages/pixelmanipulator-web-demo/src/components/PixelCounterT.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {computed, ref} from 'vue'
+import { computed } from 'vue'
 import { storeToRefs } from 'pinia'
 const props = defineProps([
 	// TODO: move state into better place
@@ -9,11 +9,10 @@ const props = defineProps([
 ]);
 const pixelCounterTStore = props.usePixelCounterTStore()
 const { checked } = storeToRefs(pixelCounterTStore)
-const pixelCounterT = ref()
 </script>
 <template>
 	<div class="form-check">
-		<input class="form-check-input" value="" ref="pixelCounterT" id="pixelCounterT" v-model="checked" type="checkbox" @change="props.change(checked)">
+		<input class="form-check-input" id="pixelCounterT" v-model="checked" type="checkbox" @change="props.change(checked)">
 		<label class="form-check-label" for="pixelCounterT" title="Show PixelCounter">
 			<i class="fa-solid fa-list-ol" aria-hidden="true"></i>
 			PixelCounter
---
+++ packages/pixelmanipulator-web-demo/src/components/ShFocusBox.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { storeToRefs } from 'pinia'
+const props = defineProps([
+	"useShFocusBoxStore"
+])
+const shFocusBoxStore = props.useShFocusBoxStore()
+const { checked } = storeToRefs(shFocusBoxStore)
+</script>
+<template>
+	<div class="form-check">
+		<input class="form-check-input" id="shfocusbox" v-model="checked" type="checkbox">
+		<label class="form-check-label" for="shfocusbox" title="Show focus box">
+			<i class="fa-solid fa-square" aria-hidden="true"></i>
+			Focus Box
+		</label>
+	</div>
+</template>
