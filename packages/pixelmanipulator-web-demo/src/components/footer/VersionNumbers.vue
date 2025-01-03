e5eb73e79c8867e57670f39c0d9338d003344d58
Lazerbeak12345 <Lazerbeak12345@users.noreply.github.com>
2025-01-03 13:01:27 -0700

    style(web-demo): organize components into folders

--- packages/pixelmanipulator-web-demo/src/components/footer/VersionNumbers.vue
+++ packages/pixelmanipulator-web-demo/src/components/footer/VersionNumbers.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { version as pm_version } from 'pixelmanipulator'
-import * as package_json from '../../package.json'
+import * as package_json from '../../../package.json'
 </script>
 <template>
 	Library v{{pm_version}},
