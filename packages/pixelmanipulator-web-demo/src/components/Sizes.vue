1552356e82b08ce5ef498ae43cf1207878c07282
Lazerbeak12345 <Lazerbeak12345@users.noreply.github.com>
2025-01-02 23:43:00 -0700

    feat(web-demo): Sizes

---
+++ packages/pixelmanipulator-web-demo/src/components/Sizes.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { storeToRefs } from 'pinia'
+const props = defineProps([
+	// TODO: move state into better place
+	'useSizesStore',
+])
+const sizesStore = props.useSizesStore()
+const { w, h } = storeToRefs(sizesStore)
+</script>
+<template>
+	<div>
+		<i class="fa-solid fa-arrows" aria-hidden="true"></i>
+		Size:
+		<div class="input-group">
+			<input
+				class="form-control"
+				id="width"
+				type="number"
+				v-model="w"
+				placeholder="150"
+				title="Default:150"/>
+			<span class="input-group-text">by</span>
+			<input
+				class="form-control"
+				id="height"
+				type="number"
+				v-model="h"
+				placeholder="150"
+				title="Default:150"/>
+			<span class="input-group-text">px</span>
+		</div>
+	</div>
+</template>
