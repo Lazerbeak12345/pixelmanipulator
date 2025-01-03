806a459576783c63987b1bc6da3833e2a05e9536
Lazerbeak12345 <Lazerbeak12345@users.noreply.github.com>
2025-01-03 15:43:58 -0700

    feat(web-demo): CustomizeName

---
+++ packages/pixelmanipulator-web-demo/src/components/CustomizeName.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { watch } from 'vue'
+import { storeToRefs } from 'pinia'
+const props = defineProps([
+	// TODO: move state into better place
+	'useCustomizeNameStore',
+	// TODO: convert to emit
+	'change',
+])
+const customizeNameStore = props.useCustomizeNameStore()
+const { name } = storeToRefs(customizeNameStore)
+watch(name, name=>props.change(name))
+</script>
+<template>
+	<label>
+		<i class="fa-solid fa-tag" aria-hidden="true"></i>
+		Change Name:
+		<input type="text" class="form-control" v-model="name">
+	</label>
+</template>
