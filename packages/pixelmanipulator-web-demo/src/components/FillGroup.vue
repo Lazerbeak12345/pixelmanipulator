17c679cf5deee0038d833dbaf5710bd75958c03a
Lazerbeak12345 <Lazerbeak12345@users.noreply.github.com>
2025-01-04 16:47:09 -0700

    feat(web-demo): FillGroup & etc.

---
+++ packages/pixelmanipulator-web-demo/src/components/FillGroup.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { watch, ref } from 'vue'
+import { storeToRefs } from 'pinia'
+
+import ElmSelect from './ElmSelect.vue'
+import FillButton from './FillButton.vue'
+import FillP from './FillP.vue'
+
+const props = defineProps([
+	// TODO: move state into better place
+	'useStore',
+	// TODO: move state into better place
+	'useElementsStore',
+	// TODO: convert to emit
+	'click',
+])
+const store = props.useStore()
+const elementsStore = props.useElementsStore()
+const { percent, selected } = storeToRefs(store)
+const { elements } = storeToRefs(elementsStore)
+</script>
+<template>
+	<div class="click-type-random input-group">
+		<ElmSelect v-model="selected" :elements />
+		<FillButton @click="props.click(selected, percent)" />
+		<FillP v-model="percent" />
+		<span class="input-group-text">%</span>
+	</div>
+</template>
--- packages/pixelmanipulator-web-demo/src/components/customize/ElementCustomize.vue
+++ packages/pixelmanipulator-web-demo/src/components/customize/ElementCustomize.vue
@@ -2,7 +2,7 @@
 import { watch } from 'vue'
 import { storeToRefs } from 'pinia'
 
-import CustomSelect from './CustomSelect.vue'
+import ElmSelect from '../ElmSelect.vue'
 import CustomizeColor from './CustomizeColor.vue'
 import CustomColorAlpha from './CustomColorAlpha.vue'
 import CustomizeName from './CustomizeName.vue'
@@ -10,6 +10,8 @@ import CustomizeName from './CustomizeName.vue'
 const props = defineProps([
 	// TODO: move state into better place
 	'useCustomizeStore',
+	// TODO: move state into better place
+	'useElementsStore',
 	// TODO: convert to emit
 	'changeSelected',
 	// TODO: convert to emit
@@ -18,13 +20,15 @@ const props = defineProps([
 	'changeName'
 ])
 const customizeStore = props.useCustomizeStore()
-const { selected, elements, color, alpha, name } = storeToRefs(customizeStore )
+const elementsStore = props.useElementsStore()
+const { selected, color, alpha, name } = storeToRefs(customizeStore)
+const { elements } = storeToRefs(elementsStore)
 watch(selected, s=>props.changeSelected(s))
 watch([color, alpha], ()=>props.changeColor())
 watch(name, name=>props.changeName(name))
 </script>
 <template>
-	<CustomSelect v-model="selected" :elements />
+	<ElmSelect v-model="selected" :elements />
 	<CustomizeColor v-model="color" />
 	<CustomColorAlpha v-model="alpha" />
 	<CustomizeName v-model="name" />
